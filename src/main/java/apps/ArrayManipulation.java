6dfa4f55cce2326c439e72ad2b06afe8b3312b18
D00271292 <d00271292@student.dkit.ie>
2025-09-24 12:06:57 +0100

    feat: printing max and min

--- src/main/java/apps/ArrayManipulation.java
+++ src/main/java/apps/ArrayManipulation.java
@@ -4,13 +4,26 @@ import numberInput.InputUtility;
 import utils.ArrayUtils;
 
 import javax.management.BadStringOperationException;
+import java.util.Scanner;
 
 public class ArrayManipulation {
     static void main() {
+        Scanner input = new Scanner(System.in);
         int[] intArray = new int[0];
         for (int i = 0; i < 10; i++) {
             intArray[i] = InputUtility.getValidInteger("Please enter an integer:", 1, 100);
         }
         ArrayUtils.displayArray(intArray);
+        System.out.println(ArrayUtils.findMax(intArray));
+        System.out.println(ArrayUtils.findMin(intArray));
+
+        String[] stringArray = new String[0];
+        for (int i = 0; i < 10; i++) {
+            stringArray[i] = input.nextLine();
+        }
+
+        System.out.println(ArrayUtils.findMax(stringArray));
+        System.out.println(ArrayUtils.findMin(stringArray));
+
     }
 }
